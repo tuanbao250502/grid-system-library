19d7d3852b9e78644030784850369e5fc4e8bb4e
tuanbao250502 <baoabcabc1234343@gmail.com>
2021-11-13 16:08:48 +0700

    upload
    grid system tiện cho việc tạo layout và căn chỉnh

---
+++ Grid system.bat
@@ -0,0 +1,21 @@
+#grid system
+
+-sắp xếp bố cục theo trật tự/ kiến trúc/ cân bằng
+3: hệ thống lưới:
+    -mutilcolumn grid
+    -single column grid
+    -modular grid
+    -baseline grid
+4:vai trò:
+-tổ chức
+-cân bằng
+-tách biện thành phần
+5: ứng dụng:
+responsive UI UX;
+6:respon web design
+ -grid: thành phần cha
+ -row:dòng
+ -column:cột
+ -grid gutter:khoảng cách 2 phía của column
+
+1.tạo class
